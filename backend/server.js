b80d7f3e815e23699d0472cbb61ddb85f6cd59eb
Marcin Mierzejewski <mmierzejewski0113@gmail.com>
2023-10-05 18:39:10 +0200

    Express and React vite app setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+//require('dotenv').config();
+
+const express = require('express');
+
+//routes files
+
+const cors = require('cors');
+const app = express();
+
+const corsOptions = {
+  exposedHeaders: 'response-header',
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// routes
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("Listening on port 4000");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <p>Tutaj kod</p>
+    </>
+  )
+}
+
+export default App
