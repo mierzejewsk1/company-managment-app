c4d2af9e73bb9d3568d022efa7fe1d8844242e05
Marcin Mierzejewski <mmierzejewski0113@gmail.com>
2023-10-07 13:09:04 +0200

    Added routing to current pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,48 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { useEffect } from "react";
+import { LOCAL_STORAGE } from "./config/Enum";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import GetNewPassword from "./pages/GetNewPassword";
+
 
 function App() {
+  const { user, isRestoreFinished, authActions } = useAuthContext();
+
+  useEffect(() => {
+    authActions.restore();
+  }, []);
 
   return (
-    <>
-      <p>Tutaj kod</p>
-    </>
+    <div className="App font-rem">
+      <BrowserRouter>
+        {isRestoreFinished ? (
+          <Routes>
+            <Route
+              path="/"
+              element={!user ? <Login /> : <Navigate to="/home" />}
+            />
+            <Route
+              path="/reset-password"
+              element={!user ? <ResetPassword /> : <Navigate to="/home" />}
+            />
+            <Route
+              path="/get-new-password"
+              element={!user ? <GetNewPassword /> : <Navigate to="/home" />}
+            />
+            <Route
+              path="/home"
+              element={user ? <Home /> : <Navigate to="/" />}
+            />
+            <Route
+              path="*"
+              element={user ? <Navigate to="/" /> : null}
+            />
+          </Routes>
+        ) : null}    {/* Loader page */}
+      </BrowserRouter>
+    </div>
   )
 }
 
