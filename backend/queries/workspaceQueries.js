dd086ac7e35c9670e456cc8f9b1e9e743121a485
Marcin Mierzejewski <mmierzejewski0113@gmail.com>
2023-10-23 18:39:59 +0200

    Added functionallity to change availability of workspace

--- backend/queries/workspaceQueries.js
+++ backend/queries/workspaceQueries.js
@@ -1,14 +1,14 @@
 const { mysql } = require('../lib/mysql');
 
 const FindWorkspaces = async () => {
-  let query = `SELECT w.workspaceID, w.userID, w.workspaceNumber, u.userName, c.userTypeName
+  let query = `SELECT w.workspaceID, w.userID, w.workspaceNumber, w.isAvailable, u.userName, c.userTypeName
     FROM o_workspaces w LEFT JOIN o_users u ON w.userID = u.userID LEFT JOIN c_user_types c ON u.userTypeID = c.userTypeID;
     `;
   return await mysql.app.select(query);
 }
 
 const FindWorkspaceByWorkspaceId = async (workspaceID) => {
-  let query = `SELECT w.workspaceID, w.userID, w.workspaceNumber, u.userName, c.userTypeName
+  let query = `SELECT w.workspaceID, w.userID, w.workspaceNumber, w.isAvailable, u.userName, c.userTypeName
     FROM o_workspaces w LEFT JOIN o_users u ON w.userID = u.userID LEFT JOIN c_user_types c ON u.userTypeID = c.userTypeID
     WHERE w.workspaceID = ?;
     `;
@@ -45,10 +45,21 @@ const RemoveEmployeeFromWorkspace = async (workspaceID) => {
   return await mysql.app.update(query, values);
 }
 
+const UpdateWorkspaceAvailability = async (workspaceID, availableValue) => {
+  let query = `
+    UPDATE o_workspaces
+    SET isAvailable = ? 
+    WHERE workspaceID = ?
+  `;
+  let values = [availableValue, workspaceID];
+  return await mysql.app.update(query, values);
+}
+
 module.exports = {
   FindWorkspaces,
   FindWorkspaceByWorkspaceId,
   FindUserWorkspaceAmount,
   UpdateWorkspace,
   RemoveEmployeeFromWorkspace,
+  UpdateWorkspaceAvailability
 }
