17c1292b80b9210540027595e513cb36eda558aa
Marcin Mierzejewski <mmierzejewski0113@gmail.com>
2023-11-06 10:35:54 +0100

    design improvements

--- frontend/src/pages/GetNewPassword.jsx
+++ frontend/src/pages/GetNewPassword.jsx
@@ -8,12 +8,13 @@ const GetNewPassword = () => {
   const resetToken = query.get('token');
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
+  const [msg, setMsg] = useState("");
 
   const ResetPassword = async (e) => {
     e.preventDefault();
     const response = await fetchData("/user/reset-password", "PATCH", { resetToken, password, password2 }, null, false);
     if (response[1] !== SERVER_CODE.OK) {
-      console.log(response[2]);
+      setMsg(response[2]);
     } else {
       window.location.href = '/';
     }
@@ -22,14 +23,19 @@ const GetNewPassword = () => {
 
   };
   return (
-    <div className="bg-gradient-to-r from-sky-300 to-sky-900 h-screen flex justify-center items-center w-screen">
-      <div className="flex-col items-center justify-center h-fit max-h-full px-5 py-12 border rounded-xl w-4/5 md:w-3/5 lg:w-2/5 xl:w-1/5">
-        <h2 className="mb-10 text-4xl">Zmiana hasła</h2>
+    <div className="bg-gray-400 h-screen flex justify-center items-center w-screen">
+      <div className="flex-col items-center justify-center h-fit max-h-full px-5 py-12 border rounded-xl w-4/5 md:w-3/5 lg:w-2/5 xl:w-[400px] bg-gray-200 border border-gray-400">
+        <h2 className="mb-10 text-3xl text-center">Zmiana hasła</h2>
         <form className="w-full" onSubmit={ResetPassword}>
-          <label className="w-full my-2 text-white" htmlFor="password">Nowe hasło: </label>
+          {msg && (
+            <div className="w-full p-3 bg-red-700 rounded-md text-sm mb-5 text-gray-100 border-2 border-red-800">
+              <p>{msg}</p>
+            </div>
+          )}
+          <label className="w-full my-2 " htmlFor="password">Nowe hasło: </label>
           <br />
           <input
-            className="w-full my-2 mb-2 p-1 bg-cyan-700 outline-none text-white placeholder:text-slate-400"
+            className="ww-full my-2 mb-5 p-1 bg-gray-300 rounded-md p-2 border border-gray-400"
             type="password"
             id="password"
             name="password"
@@ -37,10 +43,10 @@ const GetNewPassword = () => {
             onChange={(e) => { setPassword(e.target.value); }}
             required />
           <br />
-          <label className="w-full my-2 text-white" htmlFor="password2">Powtórz hasło: </label>
+          <label className="w-full my-2" htmlFor="password2">Powtórz hasło: </label>
           <br />
           <input
-            className="w-full my-2 mb-10 p-1  bg-cyan-700 outline-none text-white placeholder:text-slate-400"
+            className="w-full my-2 mb-5 p-1 bg-gray-300 rounded-md p-2 border border-gray-400"
             type="password"
             id="password"
             name="password2"
@@ -48,7 +54,7 @@ const GetNewPassword = () => {
             onChange={(e) => { setPassword2(e.target.value); }}
             required />
           <br />
-          <input className="w-full mx-auto border px-2 py-1 cursor-pointer" type="submit" value="Zmień" />
+          <input className="w-full mx-auto border px-2 py-2 cursor-pointer bg-green-600 hover:bg-green-700 duration-200 rounded-md text-gray-200" type="submit" value="Zmień" />
         </form>
       </div>
     </div>
